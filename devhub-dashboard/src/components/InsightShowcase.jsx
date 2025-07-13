6356672d5d37dc7a1ec03ca9ada6729c710d364a
Smit Bharvadiya <smitbharvadiya@gmail.com>
2025-07-13 11:36:44 +0530

    InsightShowcase

---
+++ devhub-dashboard/src/components/InsightShowcase.jsx
@@ -0,0 +1,28 @@
+import { FiArrowRight } from "react-icons/fi";
+import Carousel from "./ReactBit/Carousel";
+
+const InsightShowcase = () => {
+    return (
+        <section className="bg-white text-gray-800 mb-20 px-6 md:px-16 lg:px-24 flex flex-col md:flex-row items-center justify-between">
+
+            <Carousel />
+
+            {/* Right Side - Text */}
+            <div className="w-full lg:w-[55%] space-y-6">
+                <h2 className="text-4xl md:text-5xl font-bold leading-tight">
+                    <span className="bg-gradient-to-r from-blue-600 via-purple-600 to-indigo-600 bg-clip-text text-transparent drop-shadow-md">
+                        GitHub
+                    </span>{" "}    
+                    <span className="text-gray-800">Insights, Visualized</span>
+                </h2>
+
+                <p className="text-lg text-gray-600 leading-relaxed">
+                    DevHub empowers you to analyze GitHub activity, visualize your stack,
+                    explore repositories, and collaborate with the community — all from a single dashboard.
+                </p>
+            </div>
+        </section>
+    );
+};
+
+export default InsightShowcase;
--- devhub-dashboard/src/pages/Hometemp.jsx
+++ devhub-dashboard/src/pages/Hometemp.jsx
@@ -1,6 +1,7 @@
 import Navbar from '../components/navbar';
 import SearchInput from '../components/searchBox';
 import Footer from '../components/footer';
+import InsightShowcase from '../components/InsightShowcase';
 
 export default function Home() {
 
@@ -8,6 +9,7 @@ export default function Home() {
     <>
       <Navbar/>
       <SearchInput/>
+      <InsightShowcase/>
       <Footer />
     </>
     
